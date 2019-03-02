11b5cd5b9a6f770c5141077589f4adbdace9faaa
xl <xl@github.com>
2019-03-02 11:16:41 +0800

    feat(script) parse node info

---
+++ deploy_gdacc/script/parsenode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+output = open('static-nodes.json', 'w')
+nodes = []
+for line in open("temp-nodes.txt","r"):
+	info = line.replace('\n', '').replace('\r', '')
+	s1 = info.split(' ')
+	s2 = s1[0].split('[::]')
+	n = '  \"'+s2[0] + s1[1] + s2[1] +'\"'
+	nodes.append(n)	
+	print n
+ret = ',\n'.join(nodes)
+output.write('[\n')
+output.write(ret)
+output.write('\n]')
+output.close()	
