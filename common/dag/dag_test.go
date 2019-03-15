ecef4d69ea170b7b36010cebaa32b2e7911601a3
xl <xl@github.com>
2019-03-15 16:18:11 +0800

    feat(common)add dag pispatcher

--- common/dag/dag_test.go
+++ common/dag/dag_test.go
@@ -1,19 +1,19 @@
-// Copyright (C) 2017 go-nebulas authors
+// Copyright (C) 2017 go-dacc authors
 //
-// This file is part of the go-nebulas library.
+// This file is part of the go-dacc library.
 //
-// the go-nebulas library is free software: you can redistribute it and/or modify
+// the go-dacc library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// the go-nebulas library is distributed in the hope that it will be useful,
+// the go-dacc library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+// along with the go-dacc library.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 package dag
@@ -22,7 +22,7 @@ import (
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
-	"github.com/nebulasio/go-nebulas/common/dag/pb"
+	"github.com/daccproject/go-dacc/common/dag/pb"
 	"github.com/stretchr/testify/assert"
 )
 
